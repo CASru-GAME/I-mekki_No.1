a670a768e3323fbc5622fc19e0636e5cf7de1dfb
Abiyko <abiyko.syumi002@gmail.com>
2025-03-11 13:20:43 +0900

    衝突判定の追加
    ジャンプ後床にぶつかるまで落下
    "Wall"タグを持つオブジェクトにぶつかると反転

--- Assets/App/Scripts/Game/Enemy/EnemyGoStraight.cs
+++ Assets/App/Scripts/Game/Enemy/EnemyGoStraight.cs
@@ -5,13 +5,45 @@ namespace App.Scripts.Game.Enemy.Issue3
     public class EnemyGoStraight : MonoBehaviour
     {
         [SerializeField] private float HorizontalSpeed = 1.0f;
-        private float t = 0f;
+        private int direction = 1;
+        private float tX = 0f;
+        private float startPositionX;
+        private float startPositionY;
+        private float positionX ;
+        private float positionY ;
+        Rigidbody2D rb;
 
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            Vector2 posi = transform.position;
+            startPositionX = posi.x;
+            startPositionY = posi.y;
+            positionX = startPositionX;
+            positionY = startPositionY;
+
+        }
         void FixedUpdate()
         {
-            t += Time.deltaTime;
-            float x = HorizontalSpeed * t;
-            transform.position = new Vector2(x, 0f);
+            UpdatePosition();
+        }
+
+        void UpdatePosition()
+        {
+            tX += Time.fixedDeltaTime;
+            positionX = startPositionX + direction * HorizontalSpeed * tX;
+            Vector2 movementEnemyGoStraight = new Vector2(positionX, positionY);
+            transform.position = movementEnemyGoStraight;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Wall"))
+            {
+                tX = 0f; 
+                startPositionX = positionX;
+                direction = direction * -1;
+            }
         }
     }
 }
--- Assets/App/Scripts/Game/Enemy/EnemyJump.cs
+++ Assets/App/Scripts/Game/Enemy/EnemyJump.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace App.Scripts.Game.Enemy.Issue3
@@ -7,13 +8,69 @@ namespace App.Scripts.Game.Enemy.Issue3
         [SerializeField] private float HorizontalSpeed = 1.0f;
         [SerializeField] private float JumpHeight = 1.0f;
         [SerializeField] private float JumpPitch = 1.0f;
-        private float t = 0f;
+        private int direction = 1;
+        private float tX = 0f;
+        private float tY = 0f;
+        private float startPositionX;
+        private float startPositionY;
+        private float positionX ;
+        private float positionY ;
+        Rigidbody2D rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            Vector2 posi = transform.position;
+            startPositionX = posi.x;
+            startPositionY = posi.y;
+            positionX = startPositionX;
+            positionY = startPositionY;
+
+        }
         void FixedUpdate()
         {
-            t += Time.fixedDeltaTime;
-            Vector2 movementEnemyJump = new Vector2(HorizontalSpeed * t, JumpHeight * Mathf.Abs(Mathf.Sin(JumpPitch * t)));
-            transform.position = movementEnemyJump;
+            UpdatePosition();
         }
 
+        void UpdatePosition()
+        {
+            tX += Time.fixedDeltaTime;
+            tY += Time.fixedDeltaTime;
+            if (positionY < startPositionY)
+            {
+                positionX = startPositionX + direction * HorizontalSpeed * tX;
+                positionY = startPositionY - JumpHeight * JumpPitch * (tY - Mathf.PI / JumpPitch) - JumpHeight * Mathf.Pow(JumpPitch, 2) / 2 * Mathf.Pow((tY - Mathf.PI / JumpPitch), 2);
+                Vector2 movementEnemyJump = new Vector2(positionX, positionY);
+                transform.position = movementEnemyJump;
+            }
+            else
+            {
+                positionX = startPositionX + direction * HorizontalSpeed * tX;
+                positionY = startPositionY + JumpHeight* Mathf.Sin(JumpPitch * tY);
+                Vector2 movementEnemyJump = new Vector2(positionX, positionY);
+                transform.position = movementEnemyJump;
+                positionY = movementEnemyJump.y;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Wall"))
+            {
+                tX = 0f; 
+                startPositionX = positionX;
+                direction = direction * -1;
+            }
+            else if(tY < Mathf.PI / (2 * JumpPitch))
+            {
+                tY = Mathf.PI / JumpPitch - tY;
+            }
+            else
+            {
+                tY = 0f;
+                startPositionY = positionY;
+                
+            }
+        }
     }
 }
