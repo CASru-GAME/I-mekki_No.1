be4f43762a714dbf6cd8399bb3ed332045b74951
Ryo319 <ryou132066@gmail.com>
2025-04-03 22:24:24 +0900

    試行錯誤中

--- Assets/App/Scripts/Game/Player/Move/Dash.cs
+++ Assets/App/Scripts/Game/Player/Move/Dash.cs
@@ -31,13 +31,21 @@ namespace App.Scripts.Game.Player.Move
             {
                 canDash = false; // ダッシュを使用したのでフラグを無効にする
                 jump.CancelJump(); // ジャンプをキャンセル
-                rb.transform.DOMoveX(rb.transform.position.x + 1f, 0.5f)
-                .SetLoops(2, LoopType.Yoyo)
+
+                // DOTween.Toを使用して速度をアニメーション化
+                /*float dashDistance = 101f; // ダッシュの距離
+                float dashDuration = 20f; // ダッシュの持続時間
+                Vector2 startVelocity = rb.linearVelocity;
+                Vector2 endVelocity = new Vector2(dashDistance / dashDuration, rb.linearVelocity.y);
+
+                DOTween.To(() => rb.linearVelocity, x => rb.linearVelocity = x, endVelocity, dashDuration)
+                    .SetEase(Ease.OutCirc)
+                    .OnComplete(() => rb.linearVelocity = new Vector2(0, rb.linearVelocity.y)); // ダッシュ終了後に速度をゼロに設定
+*/             
+                rb.transform.DOMoveX(rb.transform.position.x + 1f, 0.25f)
+                //.SetLoops(2, LoopType.Yoyo)
                 .SetEase(Ease.OutCirc);
-                /*rb.transform.DOMoveX(rb.transform.position.x + 1f, 0.5f)
-                .SetLoops(1, LoopType.Yoyo)
-                    .SetEase(Ease.OutCirc);*/
-                    
+
                 await FixYAxisForDuration(airTime);
             }
         }
@@ -64,7 +72,6 @@ namespace App.Scripts.Game.Player.Move
             {
                 // y軸を固定するために速度をゼロに設定
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
-                
             }
 
             // 地面に着いたらダッシュを再度使用可能にする
