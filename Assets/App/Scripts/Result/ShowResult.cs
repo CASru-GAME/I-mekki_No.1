dc3df700a7968e432ea8edba3e8aeb08fb2776c8
NNinNinn4832 <meijiezhenger@gmail.com>
2025-04-19 12:15:23 +0900

    アニメーションをつけた

---
+++ Assets/App/Scripts/Result/ShowResult.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace App.Scripts.ShowResult
+{
+    public class ShowResult : MonoBehaviour
+    {
+        [SerializeField] private GameObject _resultTitle;
+        [SerializeField] private GameObject _resultView;
+        [SerializeField] private GameObject _goTitleButton;
+        [SerializeField] private Vector3 _resultTitlePos;
+        [SerializeField] private Vector3 _resultViewPos;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _goTitleButton.transform.localScale = Vector3.zero;
+
+            //リザルトの表示
+            _resultTitle.transform.DOMove(_resultTitle.transform.position + _resultTitlePos, 0.5f).OnComplete(() =>
+            {
+                _resultView.transform.DOMove(_resultView.transform.position + _resultViewPos, 0.5f).OnComplete(() =>
+                {
+                    //リザルトの表示が終わったら、ボタンを表示
+                    _goTitleButton.transform.DOScale(1.0f, 0.5f).SetEase(Ease.OutBounce);
+                });
+            });
+        }
+
+        public void OnclickGoTitle()
+        {
+            //タイトル場面にもどる
+        }
+    }
+}
